d8b207609b68ccd244b51d3a5411b63f2e5c745d
= <ryaninternet62@gmail.com>
2023-05-28 08:17:51 -0300

    Adicionei o componente de RadioButtom.

---
+++ src/main/java/br/com/ryan/component/radiobutton/RRadioButton.java
@@ -0,0 +1,11 @@
+package br.com.ryan.component.radiobutton;
+
+import javax.swing.*;
+
+public class RRadioButton extends JRadioButton {
+
+    public RRadioButton(final String title) {
+        setText(title);
+        setBackground(null);
+    }
+}
--- src/main/java/br/com/ryan/page/NewPage.java
+++ src/main/java/br/com/ryan/page/NewPage.java
@@ -4,16 +4,18 @@ import br.com.ryan.component.button.RButton;
 import br.com.ryan.component.checkbox.RCheckBox;
 import br.com.ryan.component.frame.RFrame;
 import br.com.ryan.component.panel.RPanel;
+import br.com.ryan.component.radiobutton.RRadioButton;
 import br.com.ryan.layout.RColor;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class NewPage {
 
     public NewPage() {
         RFrame frame = new RFrame();
-
         frame.add(panelTop(), BorderLayout.NORTH);
+        frame.add(panelBottom(), BorderLayout.SOUTH);
     }
 
     private RPanel panelTop() {
@@ -24,6 +26,26 @@ public class NewPage {
         temp.add(new RButton(btn -> {
             System.out.println(((RCheckBox) temp.getComponent(0)).isSelected());
         }, "Enviar"));
+
+        return temp;
+    }
+
+    private RPanel panelBottom() {
+        RPanel temp = new RPanel(RColor.PASTEL_ORANGE);
+        temp.setLayout(new GridLayout(1,3,2,2));
+
+        RRadioButton radioOne = new RRadioButton("Option One");
+        RRadioButton radioTwo = new RRadioButton("Option Two");
+        RRadioButton radioThree = new RRadioButton("Option Three");
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(radioOne);
+        buttonGroup.add(radioTwo);
+        buttonGroup.add(radioThree);
+
+        temp.add(radioOne);
+        temp.add(radioTwo);
+        temp.add(radioThree);
 
         return temp;
     }
